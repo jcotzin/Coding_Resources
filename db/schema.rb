f51f6f042adecbfc20c8045c03136d02214502b0
Jessica Cotzin <jcotzin@gmail.com>
2016-07-19 21:11:08 -0400

    added user_id to resources table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719014436) do
+ActiveRecord::Schema.define(version: 20160720011041) do
 
   create_table "resources", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20160719014436) do
     t.string   "rating"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
