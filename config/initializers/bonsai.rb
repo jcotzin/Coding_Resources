0751f7e05f2d1a87f44b92b82f739ab665eb27e0
jcotzin <jcotzin@gmail.com>
2016-09-16 15:49:08 -0400

    still trying..

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,9 +1,5 @@
 require 'elasticsearch/model'
 
-if Rails.env == 'production'
-  ENV['ELASTICSEARCH_URL'] = ENV['BONSAI_URL']
-end
-
-if ENV['https://tgfeixqj:zsikn2fpr3wj188b@rowan-4032356.us-east-1.bonsai.io']
-  Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['https://tgfeixqj:zsikn2fpr3wj188b@rowan-4032356.us-east-1.bonsai.io'], logs: true})
+if ENV['BONSAI_URL']
+  Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['BONSAI_URL'], logs: true})
 end
--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,4 +1,4 @@
 require "faraday"
 require "typhoeus/adapters/faraday"
 Searchkick.client = Elasticsearch::Client.new(hosts: ["127.0.0.1:9200", "127.0.0.1:9201"], retry_on_failure: true)
-ENV["ELASTICSEARCH_URL"] = ENV["https://tgfeixqj:zsikn2fpr3wj188b@rowan-4032356.us-east-1.bonsai.ioL"]
+ENV["ELASTICSEARCH_URL"] = ENV["BONSAI_URL"]
