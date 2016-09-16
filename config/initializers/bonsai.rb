ec9be7cc534bb3051f523a582bf5584a99de142d
jcotzin <jcotzin@gmail.com>
2016-09-16 15:41:45 -0400

    trying to get searchkick and elasticsearch working with bonsaigit add .

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,5 +1,9 @@
 require 'elasticsearch/model'
 
+if Rails.env == 'production'
+  ENV['ELASTICSEARCH_URL'] = ENV['BONSAI_URL']
+end
+
 if ENV['https://tgfeixqj:zsikn2fpr3wj188b@rowan-4032356.us-east-1.bonsai.io']
   Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['https://tgfeixqj:zsikn2fpr3wj188b@rowan-4032356.us-east-1.bonsai.io'], logs: true})
 end
