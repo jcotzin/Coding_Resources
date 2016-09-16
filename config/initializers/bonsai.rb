87b98d3c8aebb484da1e8af56a4d69d05a912840
jcotzin <jcotzin@gmail.com>
2016-09-16 15:33:33 -0400

    fixed yml

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,7 +1,5 @@
 require 'elasticsearch/model'
 
-ENV['ELASTICSEARCH_URL'] = ENV['BONSAI_URL']
-
-if ENV['BONSAI_URL']
-  Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['BONSAI_URL'], logs: true})
+if ENV['https://tgfeixqj:zsikn2fpr3wj188b@rowan-4032356.us-east-1.bonsai.io']
+  Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['https://tgfeixqj:zsikn2fpr3wj188b@rowan-4032356.us-east-1.bonsai.io'], logs: true})
 end
